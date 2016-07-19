d5bc8b116b5c0c97f9fa44e02eb25e682a900105
Vic Zhu <vicxizhu@gmail.com>
2016-07-19 13:09:25 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = "Vic";
+            Greet(name);
+
+            Console.WriteLine($"The current time is {DateTime.Now}");
+        }
+
+        static void Greet(string name)
+        {
+            if(String.IsNullOrEmpty(name)) return;
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+
+    }
+}
